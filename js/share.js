d5f81ac45f1a87e1364ab229663a42355f627699
Suneeta Devi <sdevi@teresol.com>
2023-07-10 12:09:40 +0500

    Added sharing button apis on blog 1.

---
+++ js/share.js
@@ -0,0 +1,27 @@
+// For Blog 1 //
+
+function shareOnFacebook() {
+    const navUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + 'https://cubestatistica.github.io/AirbnbManagementServices/blog-1.html';
+    window.open(navUrl, '_blank');
+}
+
+function shareOnTwitter() {
+    const navUrl =
+        'https://twitter.com/intent/tweet?text=' +
+        'https://cubestatistica.github.io/AirbnbManagementServices/blog-1.html';
+    window.open(navUrl, '_blank');
+}
+
+function shareOnLinkedin() {
+    const navUrl =
+        'https://www.linkedin.com/shareArticle?mini=true&url=' +
+        'https://cubestatistica.github.io/AirbnbManagementServices/blog-1.html';
+    window.open(navUrl, '_blank');
+}
+
+function shareOnWhatsapp() {
+    const navUrl =
+        'https://api.whatsapp.com/send/?text=' +
+        'https://cubestatistica.github.io/AirbnbManagementServices/blog-1.html';
+    window.open(navUrl, '_blank');
+}
