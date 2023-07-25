5f1c1566b45964a2adae93c8d906a659c9d1d9c7
SuneetaDevi <suneetadevi779@gmail.com>
2023-07-25 19:18:04 +0500

    Added field ids.

--- js/share.js
+++ js/share.js
@@ -1,3 +1,56 @@
+function sendMail() {
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    // alert("name = " + name);
+    if (firstname == "" && lastname == "" && email == "" && position == "" && company == "" && message == "") {
+        document.getElementById("error-message").innerHTML = "Above fields are required";
+        // alert("Fields are required");
+    }
+    else if (!(email.match(mailformat))) {
+        document.getElementById("error-message").innerHTML = "Kindly enter valid email address";
+        // alert("Fields are required");
+    }
+    else if (firstname != ""
+        && lastname != ""
+        && email != "" && email.match(mailformat)
+        && position != ""
+        && company != ""
+        && message != "") {
+        var params = {
+            firstname: document.getElementById("firstname").value,
+            lastname: document.getElementById("lastname").value,
+            email: document.getElementById("email").value,
+            position: document.getElementById("position").value,
+            company: document.getElementById("company").value,
+            message: document.getElementById("message").value
+        };
+        const serviceID = "service_36dx4hr";
+        const templateID = "template_wdbg4jl";
+        emailjs.send(serviceID, templateID, params)
+            .then(
+                res => {
+                    document.getElementById("firstname").value = "";
+                    document.getElementById("lastname").value = "";
+                    document.getElementById("email").value = "";
+                    document.getElementById("position").value = "";
+                    document.getElementById("message").value = "";
+                    document.getElementById("company").value = "";
+                    console.log(res);
+                    document.getElementById("error-message").style.display = "none";
+                    document.getElementById("sent-message").innerHTML = "Your Message Sent Successfully";
+                    // alert("Your Message Sent Successfully");
+                }
+            )
+            .catch((err) => console.log(err));
+    }
+}
+
+
+
 // For Blog 1 //
 
 function shareOnFacebook() {
@@ -57,8 +110,8 @@ function shareOnWhatsapp2() {
 // For Blog 3 //
 
 function shareOnFacebook3() {
-    const navUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + 
-    'https://cubestatistica.github.io/AirbnbManagementServices/blog-3.html';
+    const navUrl = 'https://www.facebook.com/sharer/sharer.php?u=' +
+        'https://cubestatistica.github.io/AirbnbManagementServices/blog-3.html';
     window.open(navUrl, '_blank');
 }
 
@@ -86,8 +139,8 @@ function shareOnWhatsapp3() {
 // For Blog 4 //
 
 function shareOnFacebook4() {
-    const navUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + 
-    'https://cubestatistica.github.io/AirbnbManagementServices/blog-4.html';
+    const navUrl = 'https://www.facebook.com/sharer/sharer.php?u=' +
+        'https://cubestatistica.github.io/AirbnbManagementServices/blog-4.html';
     window.open(navUrl, '_blank');
 }
 
@@ -115,8 +168,8 @@ function shareOnWhatsapp4() {
 // For Blog 5 //
 
 function shareOnFacebook5() {
-    const navUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + 
-    'https://cubestatistica.github.io/AirbnbManagementServices/blog-5.html';
+    const navUrl = 'https://www.facebook.com/sharer/sharer.php?u=' +
+        'https://cubestatistica.github.io/AirbnbManagementServices/blog-5.html';
     window.open(navUrl, '_blank');
 }
 
@@ -144,8 +197,8 @@ function shareOnWhatsapp5() {
 // For Blog 6 //
 
 function shareOnFacebook6() {
-    const navUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + 
-    'https://cubestatistica.github.io/AirbnbManagementServices/blog-6.html';
+    const navUrl = 'https://www.facebook.com/sharer/sharer.php?u=' +
+        'https://cubestatistica.github.io/AirbnbManagementServices/blog-6.html';
     window.open(navUrl, '_blank');
 }
 
